e0f2eb2e2b81a40163abf0015a21c4b5b0e12d18
Morgan Skillicorn <hello@morgan.games>
2020-06-19 02:46:09 +0100

    added CEF sharp chromium integration

--- R6DetailedStats/Program.cs
+++ R6DetailedStats/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using CefSharp;
+using CefSharp.OffScreen;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,26 +11,68 @@ namespace R6DetailedStats
 {
     class Program
     {
-        private static bool started = false, stop = false;
+        private static bool started = false, stop = false, browserLoaded = false;
         private const string version = "0.0.1";
         private static ConsoleKeyInfo keyInfo;
-        private const bool displayDebug = true;
+        private const bool displayDebug = false;
+        private static DiscordBot R6DiscordBot;
+        private static ChromiumWebBrowser browser;
+        private const string testurl = "https://r6.tracker.network/profile/pc/AbuseAndDesfuse/operators?seasonal=1";
+        private static string jsOutput = "null";
+
+        static void StartBrowser()
+        {
+            CefSharpSettings.SubprocessExitIfParentProcessClosed = true;
+            var settings = new CefSettings()
+            {
+                CachePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CefSharp/Cache")
+            };
+            Cef.Initialize(settings, performDependencyCheck: true, browserProcessHandler: null);
+            browser = new ChromiumWebBrowser(testurl);
+            browser.LoadingStateChanged += BrowserLoadingStateChanged;
+
+            OutputString("DEBUG", "browser initialised");
+
+            while (!browserLoaded)
+            {
+                OutputString("DEBUG", "loading browser window");
+            }
+        }
+
+        private static void BrowserLoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        {
+            if (!e.IsLoading)
+            {
+                OutputString("DEBUG", $"page {testurl} loaded");
+                browserLoaded = true;
+            }
+            else
+            {
+                OutputString("DEBUG", $"page {testurl} is loading");
+                browserLoaded = false;
+            }
+        }
 
         static void Start()
         {
             OutputString("BUILD", $"VERSION: {version}");
             OutputString("DEBUG", "start tick");
+
+            StartBrowser();
             started = true;
+
+            R6DiscordBot = new DiscordBot("https://discordapp.com/api/webhooks/723230554798948474/pRrkub-hThHh0eehV2-IyCwigZadlXzQbdmvLaAcEKOTaaa-rJUr9q3kjD-gTIHleQU7", "R6 Stats Bot", "https://cdn.discordapp.com/avatars/251440518658129923/a_62c5797990f35c8927e542037bef3031.gif");
 
             bool invalid = true;
 
             while (invalid)
             {
                 Clear();
+
                 OutputString("BUILD", "PRESS ENTER TO START");
                 Console.WriteLine();
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.Write($"INPUT {DateTime.Now.ToString("HH:mm:ss")} | ");
+                Console.Write($"{DateTime.Now.ToString("HH:mm:ss")} | ");
                 keyInfo = Console.ReadKey();
                 Console.WriteLine();
                 ConsoleKey consoleKeyPressed = keyInfo.Key;
@@ -37,6 +82,7 @@ namespace R6DetailedStats
                 {
                     case ConsoleKey.Enter:
                         OutputString("DEBUG", "enter key pressed");
+                        SendWebhookSimple($"Program started by {Environment.MachineName.ToString()}!");
                         invalid = false;
                         break;
                     default:
@@ -47,8 +93,50 @@ namespace R6DetailedStats
         }
         static void Update()
         {
+            OutputString("DEBUG", $"js out: {jsOutput}");
             OutputString("DEBUG", "update tick");
 
+            Clear();
+            OutputString("BUILD", "TYPE A PLAYERS NAME THEN PRESS <ENTER> TO SEARCH STATS");
+            Console.WriteLine();
+
+                
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write($"{DateTime.Now.ToString("HH:mm:ss")} | ");
+            string username = Console.ReadLine();
+            Console.ResetColor();
+
+            OutputString("BUILD", $"SEARCHING FOR {username}");
+            Console.WriteLine();
+
+            OutputString("BUILD", $"MOST PLAYED ATTACKER THIS SEASON: {SearchPlayerData(/*"abuseanddesfuse", "document.querySelector('#post-55 > div.post-inner > div > h1').textContent"*/)}");
+        }
+
+        private static string SearchPlayerData(/*string username, string jspath*/)
+        {
+            //browser.Load("https://r6.tracker.network/profile/pc/" + username + "/operators?seasonal=1");
+
+            System.IO.File.WriteAllText(@"H:\Desktop\output.txt", browser.GetSourceAsync().Result.ToString());
+            SendWebHookEmbed();
+
+            //string script = string.Format(jspath);
+            //browser.EvaluateScriptAsync(script).ContinueWith(x =>
+            //{
+            //    var response = x.Result;
+
+            //    if (response.Success && response.Result != null)
+            //    {
+            //        OutputString("DEBUG", "js response: " + response.Result.ToString());
+            //        return response.Result.ToString();
+            //    }
+            //    else
+            //    {
+            //        OutputString("DEBUG", "error!");
+            //        return null;
+            //    }
+            //});
+
+            return null;
         }
 
         static void Main(string[] args)
@@ -68,6 +156,35 @@ namespace R6DetailedStats
             Environment.Exit(0);
         }
 
+        static string JavaQuery(string script)
+        {
+            string output = "";
+
+            var scriptTask = browser.EvaluateScriptAsync(script);
+            scriptTask.ContinueWith(u =>
+            {
+                if (u.Result.Success && u.Result.Result != null)
+                {
+                    var response = u.Result.Result;
+                    output = response.ToString();
+                }
+            });
+
+            return output;
+        }
+
+        static void SendWebhookSimple(string messagetext)
+        {
+            R6DiscordBot.SendDiscordWebHookSimple(messagetext);
+            OutputString("DEBUG", $"sent simple webhook: {messagetext}");
+        }
+
+        static void SendWebHookEmbed()
+        {
+            R6DiscordBot.SendDiscordWebHookEmbeded("https://ubisoft-avatars.akamaized.net/28667467-87e8-4aa1-98de-9bdc047cd022/default_256_256.png", "R6 Detailed Stats: AbuseAndDesfuse", "https://morgan.games/", new DiscordEmbedField("top seasonal attackers", "GRIDLOCK, ACE"), new DiscordEmbedField("top seasonal defenders", "MAESTRO, KAID"), new DiscordEmbedField("rank", "Silver II (2,464 MMR)"), new DiscordEmbedField("last match info", "DEFEAT (K/D 0.40)"));
+            OutputString("DEBUG", $"sent embedded webhook");
+        }
+
         static void OutputString(string type, string msg)
         {
             if (type.Contains("DEBUG") && !displayDebug)
@@ -79,11 +196,11 @@ namespace R6DetailedStats
                 if (type.Contains("DEBUG"))
                     Console.ForegroundColor = ConsoleColor.DarkGray;
                 else if (type.Contains("BUILD"))
-                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.ForegroundColor = ConsoleColor.White;
                 else if (type.Contains("INPUT"))
                     Console.ForegroundColor = ConsoleColor.Green;
 
-                Console.WriteLine($"{type} {DateTime.Now.ToString("HH:mm:ss")} | {msg}");
+                Console.WriteLine($"{DateTime.Now.ToString("HH:mm:ss")} | {msg}");
             }
             Console.ResetColor();
         }
