0579c0e4cc31eb0605c9fbbdcfae64bdc1b09f5f
Morgan Skillicorn <hello@morgan.games>
2020-06-23 03:19:02 +0100

    now has ability to search player ops

--- R6DetailedStats/Program.cs
+++ R6DetailedStats/Program.cs
@@ -43,7 +43,6 @@ namespace R6DetailedStats
             await _client.StartAsync();
 
             await Task.Delay(-1);
-
         }
 
         private Task _client_Log(LogMessage arg)
@@ -69,67 +68,11 @@ namespace R6DetailedStats
             Console.WriteLine(message.ToString());
 
             int argPos = 0;
-            if (message.HasStringPrefix("!", ref argPos))
-            {
-                var result = await _commands.ExecuteAsync(context, argPos, _services);
-                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
-            }
-            else
-            {
-                //await context.Channel.SendMessageAsync(message.ToString());
 
-                checkFullStats(message.ToString(), context, "Couldn't find user: " + message.ToString());
-            
-            }
-        }
-
-        public static async void checkFullStats(string username, SocketCommandContext context, string errorMssg)
-        {
-            try
-            {
-                HttpClient httpClient = new HttpClient();
-                var html = await httpClient.GetStringAsync("https://r6.tracker.network/profile/pc/" + username);
-                HtmlDocument page1 = new HtmlDocument();
-                page1.LoadHtml(html);
-
-                html = await httpClient.GetStringAsync("https://r6.tracker.network/profile/pc/" + username + "/operators?seasonal=1");
-                HtmlDocument page2 = new HtmlDocument();
-                page2.LoadHtml(html);
-
-                string KD = page1.DocumentNode.SelectSingleNode("//*[@id='profile']/div[3]/div[2]/div[1]/div[2]/div/span[2]").InnerText;
-                string HighestMMR = page1.DocumentNode.SelectSingleNode("//*[@id='profile']/div[3]/div[2]/div[3]/div[2]/div[1]/div[2]/div[2]/text()").InnerText;
-                string CurrentMMR = page1.DocumentNode.SelectSingleNode("//*[@id='profile']/div[3]/div[2]/div[1]/div[1]/div/div[2]/div[1]/div[2]").InnerText;
-                string attack = "not found";
-                string defend = "not found";
-
-                if (page2.DocumentNode.SelectSingleNode("//*[@id='operators-Attackers']/tbody/tr[1]/td[1]/span") != null)
-                    attack = page2.DocumentNode.SelectSingleNode("//*[@id='operators-Attackers']/tbody/tr[1]/td[1]/span").InnerText;
-                if (page2.DocumentNode.SelectSingleNode("//*[@id='operators-Defenders']/tbody/tr[1]/td[1]/span") != null)
-                    defend = page2.DocumentNode.SelectSingleNode("//*[@id='operators-Defenders']/tbody/tr[1]/td[1]/span").InnerText;
-
-                EmbedBuilder embed = new EmbedBuilder();
-
-                embed.AddField("Seasonal KD",
-                    KD)
-                    .WithFooter(footer => footer.Text = "R6 stats powered by Morgan Bot")
-                    .WithColor(Color.Red)
-                    .WithTitle("R6 Stats: " + username)
-                    .WithDescription("SEASONAL STATS FOR " + username.ToUpper())
-                    .WithUrl("https://r6.tracker.network/profile/pc/" + username)
-                    .WithCurrentTimestamp()
-                    .Build();
-                embed.AddField("Top Attackers: ", attack);
-                embed.AddField("Top Defenders: ", defend);
-                embed.AddField("Highest MMR: ", HighestMMR);
-                embed.AddField("Current MMR: ", CurrentMMR);
-
-                await context.Channel.SendMessageAsync(null, false, embed.Build(), null);
-            }
-            catch(Exception ex)
-            {
-                await context.Channel.SendMessageAsync(errorMssg);
-                Console.WriteLine(ex.ToString());
-            }
+            await _commands.ExecuteAsync(
+            context: context,
+            argPos: argPos,
+            services: null);
         }
     }
 }
--- R6DetailedStats/commands.cs
+++ R6DetailedStats/commands.cs
@@ -9,18 +9,64 @@ using Discord;
 
 namespace R6DetailedStats
 {
+    [Group("!r6")]
     public class commands : ModuleBase<SocketCommandContext>
-    {   
-        [Command("hello")]
-        public async Task hello()
+    {
+        [Command("help")]
+        public async Task help()
         {
-            await ReplyAsync("YEET");
+            await ReplyAsync("!m commands");
         }
 
-        [Command("cunt")]
-        public async Task cunt()
+        [Command("main")]
+        public async Task main(string username, string platform = "pc")
         {
-            await ReplyAsync("Did you mean \"Corey Swankie\"?");
+            HTMLDataParser parser = new HTMLDataParser();
+
+            if (await parser.UpdateDocAsync("https://r6.tracker.network/profile/" + platform + "/" + username + "/operators?seasonal=1"))
+                Console.WriteLine("HTML doc updated.");
+            else
+                Console.WriteLine("Couldn't update HTML doc!");
+
+            string profilePic = parser.ParseImgUrl("//*[@id='profile']/div[1]/div/div[1]/img");
+            string attackSeason1 = parser.ParseString("//*[@id='operators-Attackers']/tbody/tr[1]/td[1]/span");
+            string attackSeason2 = parser.ParseString("//*[@id='operators-Attackers']/tbody/tr[2]/td[1]/span");
+            string defenseSeason1 = parser.ParseString("//*[@id='operators-Defenders']/tbody/tr[1]/td[1]/span");
+            string defenseSeason2 = parser.ParseString("//*[@id='operators-Defenders']/tbody/tr[2]/td[1]/span");
+
+
+            if (await parser.UpdateDocAsync("https://r6.tracker.network/profile/" + platform + "/" + username + "/operators"))
+                Console.WriteLine("HTML doc updated.");
+            else
+                Console.WriteLine("Couldn't update HTML doc!");
+
+            string attackOverall1 = parser.ParseString("//*[@id='operators-Attackers']/tbody/tr[1]/td[1]/span");
+            string attackOverall2 = parser.ParseString("//*[@id='operators-Attackers']/tbody/tr[2]/td[1]/span");
+            string defenseOverall1 = parser.ParseString("//*[@id='operators-Defenders']/tbody/tr[1]/td[1]/span");
+            string defenseOverall2 = parser.ParseString("//*[@id='operators-Defenders']/tbody/tr[2]/td[1]/span");
+
+            EmbedBuilder embed = new EmbedBuilder();
+
+            embed.AddField("Top Attackers (Seasonal)",
+                "1: " + attackSeason1 + "\n2: " + attackSeason2)
+                .WithFooter(footer => footer.Text = "R6 stats powered by Morgan Bot")
+                .WithColor(Color.Purple)
+                .WithTitle("Top Operators: " + username)
+                .WithUrl("https://morgan.games/discord-bot/")
+                .WithCurrentTimestamp()
+                .WithThumbnailUrl(profilePic)
+                .Build();
+
+            embed.AddField("Top Defenders (Seasonal)",
+                "1: " + defenseSeason1 + "\n2: " + defenseSeason2);
+
+            embed.AddField("Top Attackers (Overall)",
+                "1: " + attackOverall1 + "\n2: " + attackOverall2);
+
+            embed.AddField("Top Defenders (Overall)",
+                "1: " + defenseOverall1 + "\n2: " + defenseOverall2);
+
+            await ReplyAsync(null, false, embed.Build(), null);
         }
 
         [Command("embed")]
